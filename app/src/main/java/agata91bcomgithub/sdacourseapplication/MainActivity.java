988c80daf6c6d6044d4ee11f38038e03bb0f912a
Agata Bielinska <agata.bielinska@wp.pl>
2017-03-02 20:41:36 +0100

    3books and checkbox and tablayout with viewpager

--- app/src/main/java/agata91bcomgithub/sdacourseapplication/MainActivity.java
+++ app/src/main/java/agata91bcomgithub/sdacourseapplication/MainActivity.java
@@ -2,7 +2,6 @@ package agata91bcomgithub.sdacourseapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,13 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import agata91bcomgithub.sdacourseapplication.drawing.DrawingMainActivity;
 import agata91bcomgithub.sdacourseapplication.quiz.QuizActivity;
 import agata91bcomgithub.sdacourseapplication.todolist.ToDoListActivity;
+import agata91bcomgithub.sdacourseapplication.book.BooksActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String NOTES_KEY = "notes";
@@ -61,6 +58,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        TextView booksApplication = (TextView) findViewById(R.id.books_application);
+        booksApplication.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BooksActivity.class);
                 startActivity(intent);
 
             }
